b8f8fbf1d507944406ea93766b8c9c98cfd75420
Jaydev Mahadevan <jaydevm@gmail.com>
2017-10-30 10:49:20 -0500

    Create initial version of plugin

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /* eslint-env node */
 'use strict';
 
+const BasePlugin = require('ember-cli-deploy-plugin');
+const { spawn } = require('child_process');
+
 module.exports = {
-  name: 'ember-cli-deploy-nanobox-redis-tunnel'
+  name: 'ember-cli-deploy-nanobox-redis-tunnel',
+
+  createDeployPlugin(options) {
+    const DeployPlugin = BasePlugin.extend({
+      name: options.name,
+      runBefore: 'ember-cli-deploy-redis',
+      defaultConfig: {port: 6379},
+      requiredConfig: ['remote', 'component'],
+
+      willUpload() {
+        const proc = spawn('nanobox', [
+          'tunnel',
+          this.readConfig('remote'),
+          this.readConfig('component'),
+          '-p',
+          this.readConfig('port')
+        ]);
+        return {tunnel: proc};
+      },
+
+      didUpload(context) {
+        context.tunnel.kill('SIGTERM');
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
