ac589c9a73ec97ca30bed3796d3b87214c35b792
Jaydev Mahadevan <jaydevm@gmail.com>
2017-11-27 18:05:56 -0600

    Wait for Redis connection to establish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const BasePlugin = require('ember-cli-deploy-plugin');
 const { spawn } = require('child_process');
+const redis = require('redis');
 
 module.exports = {
   name: 'ember-cli-deploy-nanobox-redis-tunnel',
@@ -17,6 +18,7 @@ module.exports = {
         const remote = this.readConfig('remote'),
               component = this.readConfig('component'),
               port = this.readConfig('port');
+
         const proc = spawn('nanobox', [
           'tunnel',
           remote,
@@ -24,17 +26,45 @@ module.exports = {
           '-p',
           port
         ]);
+
         this.log(
           `Connected to component \`${component}\` on port \`${port}\` ` +
           `on remote \`${remote}\``,
           {verbose: true}
         );
-        return {tunnel: proc};
+
+        // Wait for the connection to establish before returning
+        return this._waitForConnection(port, proc);
       },
 
       teardown(context) {
         this.log('Closing Redis tunnel', {verbose: true});
         context.tunnel.kill('SIGTERM');
+      },
+
+      _waitForConnection(port, proc) {
+        return new Promise((resolve, reject) => {
+          const client = redis.createClient({
+            host: 'localhost',
+            port: port
+          });
+
+          client.on('error', err => {
+            if (err.code === 'ECONNREFUSED') {
+              this.log(
+                'Connection refused. Tunnel may not be ready. Retrying...',
+                {verbose: true}
+              );
+            } else {
+              reject(err.toString());
+            }
+          });
+
+          client.on('ready', () => {
+            this.log('Connection ready', {verbose: true});
+            resolve({tunnel: proc});
+          });
+        });
       }
     });
 
